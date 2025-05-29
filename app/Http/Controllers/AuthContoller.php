ae0af2dac14ee88dfb50ac58f1c3e46f588006e5
Miguel Chuquimia <miguel.chuquimia.leyton24@gmail.com>
2025-05-29 16:54:57 -0400

    Autenticacion Login, Registro y Perfil

--- app/Http/Controllers/AuthContoller.php
+++ app/Http/Controllers/AuthContoller.php
@@ -3,8 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthContoller extends Controller
 {
-    //
+    public function funLogin(Request $request){
+        // Validar los datos
+        $credenciales = $request->validate([
+            'email' => 'required | email',
+            'password' => 'required'
+
+        ]);
+
+        // Autenticar
+        if(!Auth::attempt($credenciales)){
+            return response()->json(['mensaje' => 'Credenciales Incorrectas'], 401);
+        }
+        
+        // Generamos un token
+        $token = $request->user()->createToken("Token auth")->plainTextToken;
+
+        return response([
+            "access_token" => $token,
+            "usuario" => $request->user()
+        ], 201);
+    }
+
+    public function funRegister(Request $request){
+        // Validar los datos
+        $request->validate([
+            'name' => 'required | max:100 | min:2 | string',
+            'email' => 'required | email | unique:users',
+            'password' => 'required | same:c_password'
+        ]);
+        
+        // Registrar en la BD
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+        $usuario->save();
+
+        // Responder
+        return response()->json(['mensaje' => 'Usuario Registrado']);
+
+    }
+
+    public function funProfile(Request $request){
+        return response()->json($request->user(), 200);
+    }
+
+    public function funLogout(Request $request){
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthContoller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('/v1/auth')->group(function(){
+
+    Route::post('/login', [AuthContoller::class, 'funLogin']);
+    Route::post('/register', [AuthContoller::class, 'funRegister']);
+    
+    Route::middleware('auth:sanctum')->group(function () { 
+        Route::get('/profile', [AuthContoller::class, 'funProfile']);
+        Route::post('/logout', [AuthContoller::class, 'funLogout']);
+    });
+});
